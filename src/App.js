5f6cfc1b14aad39ec1a02d578a7763510ae5bd91
Awais-cb <awaisfiazit@gmail.com>
2018-08-03 19:55:29 +0500

    added form to adding data to state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
@@ -7,38 +8,51 @@ class App extends Component {
   constructor(){
     // in a constructor this can't be used without calling super();
     super();
-    this.state = {
-      projects:[
-          {
-            'title':'Business Website',
-            'category':'Web design'
-          },
-          {
-            'title':'School Website',
-            'category':'Web design'
-          },
-          {
-            'title':'Park Website',
-            'category':'Data mining'
-          },
-          {
-            'title':'Hotel Website',
-            'category':'Data scrapping'
-          },
-          {
-            'title':'College Website',
-            'category':'Web development'
-          },
-        ]
+      this.state = {
+        // we actually don't add up data array directly into our state in constructor as alternative we will use this.setState
+        projects:[]
       }
-  }
+    }
+    componentWillMount(){
+        this.setState({
+          projects : [
+                    {
+                      'title':'Business Website',
+                      'category':'Web design'
+                    },
+                    {
+                      'title':'School Website',
+                      'category':'Web design'
+                    },
+                    {
+                      'title':'Park Website',
+                      'category':'Data mining'
+                    },
+                    {
+                      'title':'Hotel Website',
+                      'category':'Data scrapping'
+                    },
+                    {
+                      'title':'College Website',
+                      'category':'Web development'
+                    }
+                ]
+              });
+      }
+      handleAddProject(project){
+        // console.log(project);
+        let projects = this.state.pojects;
+        projects.push(project);
+        this.setState({projects:projects});
+      }  
   // redering content to webpage
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <header className="App-header">
           <h1 className="App-title">ReactJS demo app</h1>
         </header>
+        <AddProject addProject={this.handleAddProject.bind(this)} />
         <Projects projects={this.state.projects} />
       </div>
     );
---
+++ src/Components/AddProject.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+  constructor(){
+    super();
+    this.state={
+      newProject:{}
+    }
+  }
+  static defaultProps = {
+    categories: ['Web Design','Web Development','Mobile Development']
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    if(this.refs.title.value === '' || this.refs.category.value === ''){
+      alert('Fill in all required fields');
+    }else{
+      // console.log('submited');
+      this.setState({
+            newProject:{
+              title:this.refs.title.value,
+              category:this.refs.category.value
+            }
+      },function (argument) {
+        // console.log(this.state);
+        // sending data to main main state containing all projects in App.js
+        this.props.addProject(this.state.newProject);
+      });
+    }
+  }
+  render() {
+    let oneCategory;
+    oneCategory = this.props.categories.map((category)=>{
+        return <option key={category} value={category} >{category}</option>
+    }); 
+    return (
+      <div className="AddProject">
+        <h2>Add Project</h2>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div>
+            <div className="form-group">
+              <label htmlFor='title'>Title : </label><input className='form-control' type='text' ref='title' />
+            </div>
+            <div className="form-group">
+              <label htmlFor='category'>Category : </label>
+              <select className='form-control' ref='category'>
+                {oneCategory}
+              </select>
+            </div>
+            <input className='btn btn-dafault' type='submit' value=' Submit '/>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddProject;
